db560b981a62f60a0599f903a6396f4a141e1d1b
ManuelaKvie <132114552+ManuelaKvie@users.noreply.github.com>
2024-08-16 11:23:53 -0300

    01

---
+++ script.js
@@ -0,0 +1,8 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-principal');
+const caixaAlternativas = document.querySelector('.caixa-principal');
+const caixaResultado = document.querySelector('.caixa-principal');
+const textoResultado = document.querySelector('.caixa-principal');
+
+let atual = 0;
+let perguntaAtual;
